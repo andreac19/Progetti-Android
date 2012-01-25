3fb71249ea806ebb1710083e3d713f9d585e8341
Andrea Ceriola <polvamos87@hotmail.it>
2012-01-25 12:45:57 +0100

    progetto dueactivity

---
+++ DueActivity/src/andrea/polverone/es1/Second.java
@@ -0,0 +1,12 @@
+package andrea.polverone.es1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity{
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.second);
+	    }
+
+}
